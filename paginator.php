069ce869fee0e43668e8a3668d1512f1a3a3745b
= <=>
2016-05-20 16:17:15 +0700

    fix b,c can any but a must less 100000 #4

--- paginator.php
+++ paginator.php
@@ -1,8 +1,13 @@
 <?php 
+$page_current = isset ( $_GET["page"] ) ? intval ( $_GET["page"] ) : 1;
+if($page_current == 0){
+	$page_current = 1;
+}
 if(isset($_POST['submit'])){
 	$a = isset ( $_POST["a"] ) ? $_POST["a"] : 0;
 	$b = isset ( $_POST["b"] ) ? $_POST["b"] : 0;
 	$c = isset ( $_POST["c"] ) ? $_POST["c"] : 0;
+	$page_current = 1;
 }else{
 	$a = isset ( $_GET["a"] ) ? intval ( $_GET["a"] ) : 0;
 	$b = isset ( $_GET["b"] ) ? intval ( $_GET["b"] ) : 0;
@@ -20,33 +25,38 @@ if(isset($_POST['submit'])){
 <?php
 function pagination($a, $b, $value_number_limit, $url, $page_current){
 	$multiples = array();
-	$number = 0;
 	$i = 1;
-	while ($number < $a) {
+	while (1) {
 		$number = $i * $b;
-		$multiples[] = $number;
+		if($number < $a){
+			$multiples[] = $number;	
+		}else {
+			break;
+		}
 		$i++;
 	}
 	$total_value = sizeof($multiples);
-	$value_start = ($page_current - 1) * $value_number_limit;
-	$value_finish = $page_current * $value_number_limit;
-	if($value_finish > $total_value){
-		$value_finish = $total_value;
-	}
-	echo '<h3>Giá trị trên mỗi trang: </h3>';
-	for($j = $value_start; $j < $value_finish; $j++ ){
-		echo $multiples[$j];
-		echo '<br>';
-	}
-	echo '<hr>';
 	if($total_value > 0){
-		$list_page = '<table border = "1"><tr>';
-		$number_page = ceil($total_value / $value_number_limit);
-		if($page_current > 1){
-			$page = $page_current - 1;
-			$list_page .= '<td><a href = "'.$url.'?page='.$page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'Prev</a></td>';
+		$value_start = ($page_current - 1) * $value_number_limit;
+		$value_finish = $page_current * $value_number_limit;
+		if($value_finish > $total_value){
+			$value_finish = $total_value;
 		}
+		echo '<h3>Giá trị trên mỗi trang: </h3>';
+		for($j = $value_start; $j < $value_finish; $j++ ){
+			echo $multiples[$j];
+			echo '<br>';
+		}
+		echo '<hr>';
+		
+		$number_page = ceil($total_value / $value_number_limit);
+		
 		if($number_page > 1){
+			$list_page = '<table border = "1"><tr>';
+			if($page_current > 1){
+				$page = $page_current - 1;
+				$list_page .= '<td><a href = "'.$url.'?page='.$page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'Prev</a></td>';
+			}
 			for($i = 1; $i <= $number_page; $i++){
 				if($i == $page_current){
 					$list_page .= '<td><b style = "color: red">'.$i.'</b></td>';
@@ -54,28 +64,24 @@ function pagination($a, $b, $value_number_limit, $url, $page_current){
 					$list_page .= '<td><a href = "'.$url.'?page='.$i.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.$i.'</a></td>';
 				}
 			}
-		}
-		if($page_current < $number_page){
-			$page = $page_current + 1;
-			$list_page .= '<td><a href = "'.$url.'?page='.$page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'Next</a></td>';
-		}
-		$list_page .= "</tr></table>";
+			if($page_current < $number_page){
+				$page = $page_current + 1;
+				$list_page .= '<td><a href = "'.$url.'?page='.$page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'Next</a></td>';
+			}
+			$list_page .= "</tr></table>";
+			echo $list_page;
+		}	
 	}
-	echo $list_page;
 }
 
-$page_current = isset ( $_GET["page"] ) ? intval ( $_GET["page"] ) : 1;
-if($page_current == 0){
-	$page_current = 1;
-}
 if($a != 0 && $b != 0 && $c != 0 && $page_current > 0){
 	if(is_numeric($a) && is_numeric($b) && is_numeric($c)){
 		if((int)$a == floatval($a) && (int)$b == floatval($b) && (int)$c == floatval($c)){
-			if($b <= $a){
+			if($a < 100000){
 				$url = "paginator.php";
 				pagination($a, $b, $c, $url, $page_current);		
 			}else{
-				echo '<b style = "color: red">Giá trị b phải nhỏ hơn giá trị a</b>';
+				echo '<b style = "color: red">Giá trị a quá lớn</b>';
 			}
 		}else{
 			echo '<b style = "color: red">Bạn đã nhập sai giá trị! Vui lòng nhập số nguyên !</b>';
@@ -84,6 +90,4 @@ if($a != 0 && $b != 0 && $c != 0 && $page_current > 0){
 		echo '<b style = "color: red">Bạn đã nhập sai giá trị! Vui lòng nhập số nguyên !</b>';
 	} 
 }
-
-
 ?>
