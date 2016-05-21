876e850b38f982047a2ef7049e3bd7d032ea0a39
= <=>
2016-05-22 00:39:06 +0700

    level2

--- paginator.php
+++ paginator.php
@@ -48,28 +48,31 @@ function pagination($a, $b, $value_number_limit, $url, $page_current){
 			echo '<br>';
 		}
 		echo '<hr>';
-		
 		$number_page = ceil($total_value / $value_number_limit);
-		
 		if($number_page > 1){
 			$list_page = '<table border = "1"><tr>';
 			if($page_current > 1){
 				$page = $page_current - 1;
 				$list_page .= '<td><a href = "'.$url.'?page='.$page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'Prev</a></td>';
 			}
-			if($page_current <= 2){
-				$page_start = 1;
-				$page_finish = 5;	
-			}elseif($page_current > 2 && $page_current < $number_page - 2){
-				$page_start = $page_current - 2;
-				$page_finish = $page_current + 2;
+			if($number_page > 8){
+				if($page_current <= 4){
+					$page_start = 1;
+					$page_finish = $number_page > 5 ? 5 : $number_page;	
+				}elseif($page_current > 4 && $page_current < $number_page - 3){
+					$page_start = $page_current - 2;
+					$page_finish = $page_current + 2;
+				}else {
+					$page_start = $page_current > 4 ? $number_page - 4 : 1;
+					$page_finish = $number_page;
+				}
+				if($page_current > 4){
+					$list_page .= '<td><a href = "'.$url.'?page=1&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'1</a></td>';
+					$list_page .= "<td><b>...</b></td>";
+				}	
 			}else {
-				$page_start = $number_page - 4;
+				$page_start = 1;
 				$page_finish = $number_page;
-			}
-			if($page_current > 3){
-				$list_page .= '<td><a href = "'.$url.'?page=1&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.'1</a></td>';
-				$list_page .= "<td><b>...</b></td>";
 			}
 			for($i = $page_start; $i <= $page_finish; $i++){
 				if($i == $page_current){
@@ -78,9 +81,11 @@ function pagination($a, $b, $value_number_limit, $url, $page_current){
 					$list_page .= '<td><a href = "'.$url.'?page='.$i.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.$i.'</a></td>';
 				}
 			}
-			if($page_current < $number_page - 2){
-				$list_page .= "<td><b>...</b></td>";
-				$list_page .= '<td><a href = "'.$url.'?page='.$number_page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.$number_page.'</a></td>';
+			if($number_page > 8){
+				if($page_current < $number_page - 3){
+					$list_page .= "<td><b>...</b></td>";
+					$list_page .= '<td><a href = "'.$url.'?page='.$number_page.'&a='.$a.'&b='.$b.'&c='.$value_number_limit.'">'.$number_page.'</a></td>';
+				}
 			}
 			if($page_current < $number_page){
 				$page = $page_current + 1;
@@ -91,17 +96,29 @@ function pagination($a, $b, $value_number_limit, $url, $page_current){
 		}	
 	}
 }
-
-if($a != 0 && $b != 0 && $c != 0 && $page_current > 0){
-	if(is_numeric($a) && is_numeric($b) && is_numeric($c)){
-		if((int)$a == floatval($a) && (int)$b == floatval($b) && (int)$c == floatval($c)){
-			$url = "paginator.php";
-			pagination($a, $b, $c, $url, $page_current);		
-		}else{
+if($page_current > 0){
+	if($a != 0 && $b != 0 && $c != 0 && $page_current > 0){
+		if(is_numeric($a) && is_numeric($b) && is_numeric($c)){
+			if((int)$a == floatval($a) && (int)$b == floatval($b) && (int)$c == floatval($c)){
+				if($a > $b){
+					$url = "paginator.php";
+					pagination($a, $b, $c, $url, $page_current);	
+				}else{
+					echo '<b style = "color: blue">Không có bản ghi nào !!</b>';
+				}
+			}else{
+				echo '<b style = "color: red">Bạn đã nhập sai giá trị! Vui lòng nhập số nguyên !</b>';
+			}
+		}else {
 			echo '<b style = "color: red">Bạn đã nhập sai giá trị! Vui lòng nhập số nguyên !</b>';
-		}
+		} 
 	}else {
-		echo '<b style = "color: red">Bạn đã nhập sai giá trị! Vui lòng nhập số nguyên !</b>';
-	} 
+		if(isset($_POST['submit'])){
+			echo '<b style = "color: red">Bạn chưa nhập giá trị! Vui lòng nhập giá trị vào !</b>';
+		}
+	}	
+}else{
+	echo '<b style = "color: red">Trang không tồn tại !</b>';
 }
+
 ?>
