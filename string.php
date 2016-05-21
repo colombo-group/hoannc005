56ab9a37da72ed29bd4702a06030b850e378869b
= <=>
2016-05-21 22:17:05 +0700

    bat ky tu dac biet

--- string.php
+++ string.php
@@ -14,29 +14,34 @@ $characters = isset($_POST['characters']) ? $_POST['characters'] : "";
 if(isset($_POST["submit"])){
 	if(!empty($paragraph) && !empty($characters)){
 		$vietnamese = "ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼẾỀỂưăạảấầẩẫậắằẳẵặẹẻẽếềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ";
-		$pattern_character = "/$characters/";
-		$pattern_word = '/[a-zA-Z0-9_'.$vietnamese.']*'.$characters.'[a-zA-Z0-9_'.$vietnamese.']*/';
-		$characters_replaced = '<b style="font-size:30px">'.$characters.'</b>';
-		preg_match_all($pattern_word, $paragraph, $listWords, PREG_OFFSET_CAPTURE);
-		preg_match_all($pattern_character, $paragraph, $matches, PREG_OFFSET_CAPTURE);
-		echo '<h3>Đoạn văn bản nhập vào: </h3>';
-		echo $paragraph.'<hr>';
-		echo 'Số lần xuất hiện của ký tự "'.$characters.'" là: '.sizeof($matches[0]).'<br><hr>';
-		echo '<h3>Danh sách các từ chứa ký tự "'.$characters.'" là: </h3>';
-		$pattern_word_replaced = array();
-		$word_replaced = array();
-		for($i = 0; $i < sizeof($listWords[0]); $i++){
-			echo $i + 1; 
-			echo '- '.$listWords[0][$i][0];
-			echo '<br>';
-			$pattern_word_replaced[] = '/'.$listWords[0][$i][0].'/';
-			$word_replaced[] = '<b style="font-size:30px">'.$listWords[0][$i][0].'</b>';
+		$pattern_input = "/[^a-zA-Z0-9_$vietnamese\s]/";
+		if(!preg_match($pattern_input, $characters)){
+			$pattern_character = "/$characters/";
+			$pattern_word = '/[a-zA-Z0-9_'.$vietnamese.']*'.$characters.'[a-zA-Z0-9_'.$vietnamese.']*/';
+			$characters_replaced = '<b style="font-size:30px">'.$characters.'</b>';
+			preg_match_all($pattern_word, $paragraph, $listWords, PREG_OFFSET_CAPTURE);
+			preg_match_all($pattern_character, $paragraph, $matches, PREG_OFFSET_CAPTURE);
+			echo '<h3>Số lần xuất hiện của ký tự "'.$characters.'" là: '.sizeof($matches[0]).'</h3><hr>';
+			if(sizeof($matches[0]) > 0){
+				echo '<h3>Danh sách các từ chứa ký tự "'.$characters.'" là: </h3>';
+				$pattern_word_replaced = array();
+				$word_replaced = array();
+				for($i = 0; $i < sizeof($listWords[0]); $i++){
+					echo $i + 1; 
+					echo '- '.$listWords[0][$i][0];
+					echo '<br>';
+					$pattern_word_replaced[] = '/'.$listWords[0][$i][0].'/';
+					$word_replaced[] = '<b style="font-size:30px">'.$listWords[0][$i][0].'</b>';
+				}
+				echo '<hr><h3>Đoạn văn bản chứa ký tự "'.$characters.'" được bôi đậm: </h3>';
+				echo preg_replace($pattern_character,$characters_replaced, $paragraph);
+				echo '<hr>';
+				echo '<h3>Đoạn văn bản với các từ chứa ký tự "'.$characters.'" được bôi đậm :</h3>';
+				echo preg_replace($pattern_word_replaced, $word_replaced, $paragraph);
+			}
+		}else{
+			echo '<b style = "color: red">Bạn đã nhập ký tự không phù hợp ! Vui lòng nhập lại !</b>';
 		}
-		echo '<hr><h3>Đoạn văn bản chứa ký tự "'.$characters.'" được bôi đậm: </h3>';
-		echo preg_replace($pattern_character,$characters_replaced, $paragraph);
-		echo '<hr>';
-		echo '<h3>Đoạn văn bản với các từ chứa ký tự "'.$characters.'" được bôi đậm :</h3>';
-		echo preg_replace($pattern_word_replaced, $word_replaced, $paragraph);
 	}else{
 		echo '<b style = "color: red">Không được để trống ! Vui lòng nhập vào !</b>';
 	}	
