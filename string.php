f82ec5a3255f91a05ecdaf2a257d7cf7d52c8f05
= <=>
2016-06-14 19:29:38 +0700

    trainee008

--- string.php
+++ string.php
@@ -1,17 +1,7 @@
 <?php 
 header('Content-Type: text/html; charset=utf-8');
-$paragraph = isset($_POST['paragraph']) ? $_POST['paragraph'] : "";
-$characters = isset($_POST['characters']) ? $_POST['characters'] : "";
-?>
-<h1>Bài bonus 2</h1>
-<form action = "" method = "post">
-	Đoạn văn bản: <br>
-	<textarea name = "paragraph" rows="10" cols="100"><?php echo $paragraph;?></textarea>
-	<br>Ký tự tìm kiếm: <input type = "text" name = "characters" value = "<?php echo $characters;?>">
-	<input type = "submit" value = "Phân tích" name = "submit">
-</form>
-<?php 
-if(isset($_POST["submit"])){
+function find($paragraph, $characters){
+	$words_finded = array();
 	if(!empty($paragraph) && !empty($characters)){
 		$vietnamese = "ÀÁÂÃÈÉÊÌÍÒÓÔÕÙÚĂĐĨŨƠàáâãèéêìíòóôõùúủăđĩũơƯĂẠẢẤẦẨẪẬẮẰẲẴẶẸẺẼẾỀỂưăạảấầẩẫậắằẳẵặẹẻẽếềểỄỆỈỊỌỎỐỒỔỖỘỚỜỞỠỢỤỦỨỪễệỉịọỏốồổỗộớờởỡợụủứừỬỮỰỲỴÝỶỸửữựỳỵỷỹ";
 		$pattern_input = "/[^a-zA-Z0-9_$vietnamese\s]/";
@@ -21,29 +11,65 @@ if(isset($_POST["submit"])){
 			$characters_replaced = '<b style="font-size:30px">'.$characters.'</b>';
 			preg_match_all($pattern_word, $paragraph, $listWords, PREG_OFFSET_CAPTURE);
 			preg_match_all($pattern_character, $paragraph, $matches, PREG_OFFSET_CAPTURE);
-			echo '<h3>Số lần xuất hiện của ký tự "'.$characters.'" là: '.sizeof($matches[0]).'</h3><hr>';
-			if(sizeof($matches[0]) > 0){
-				echo '<h3>Danh sách các từ chứa ký tự "'.$characters.'" là: </h3>';
+			$number = sizeof($matches[0]);
+			if($number > 0){
 				$pattern_word_replaced = array();
 				$word_replaced = array();
 				for($i = 0; $i < sizeof($listWords[0]); $i++){
-					echo $i + 1; 
-					echo '- '.$listWords[0][$i][0];
-					echo '<br>';
+					$words_finded[] = $listWords[0][$i][0];
 					$pattern_word_replaced[] = '/'.$listWords[0][$i][0].'/';
 					$word_replaced[] = '<b style="font-size:30px">'.$listWords[0][$i][0].'</b>';
 				}
-				echo '<hr><h3>Đoạn văn bản chứa ký tự "'.$characters.'" được bôi đậm: </h3>';
-				echo preg_replace($pattern_character,$characters_replaced, $paragraph);
-				echo '<hr>';
-				echo '<h3>Đoạn văn bản với các từ chứa ký tự "'.$characters.'" được bôi đậm :</h3>';
-				echo preg_replace($pattern_word_replaced, $word_replaced, $paragraph);
+				$paragraph_characters = preg_replace($pattern_character,$characters_replaced, $paragraph);
+				$paragraph_words = preg_replace($pattern_word_replaced, $word_replaced, $paragraph);
+				return array(
+					'status' => true,
+					'count' => $number,
+					'words_finded' => $words_finded,
+					'paragraph_characters' => $paragraph_characters,
+					'paragraph_words' => $paragraph_words
+				);
+			}else{
+				$error = "Không tìm được kết quả phù hợp";
 			}
 		}else{
-			echo '<b style = "color: red">Bạn đã nhập ký tự không phù hợp ! Vui lòng nhập lại !</b>';
+			$error = "Bạn đã nhập ký tự không phù hợp ! Vui lòng nhập lại !";
 		}
 	}else{
-		echo '<b style = "color: red">Không được để trống ! Vui lòng nhập vào !</b>';
-	}	
+		$error = "Không được để trống ! Vui lòng nhập vào !";
+	}
+	return array(
+		'status' => false,
+		'error' => $error
+	);
+}
+$paragraph = isset($_POST['paragraph']) ? $_POST['paragraph'] : "";
+$characters = isset($_POST['characters']) ? $_POST['characters'] : "";
+if(isset($_POST["submit"])){
+	$result = find($paragraph, $characters);
 }
 ?>
+<h1>Bài bonus 2</h1>
+<form action = "" method = "post">
+	Đoạn văn bản: <br>
+	<textarea name = "paragraph" rows="10" cols="100"><?php echo $paragraph;?></textarea>
+	<br>Ký tự tìm kiếm: <input type = "text" name = "characters" value = "<?php echo $characters;?>">
+	<input type = "submit" value = "Phân tích" name = "submit">
+</form>
+<?php if($result['status'] == true) :?>
+<h3>Số lần xuất hiện của ký tự <?php echo $characters ?> là: <?php echo $result['count'] ?></h3><hr>
+<h3>Danh sách các từ chứa ký tự <?php echo $characters ?> là: </h3>
+<?php
+foreach ($result['words_finded'] as $key => $word){
+	echo "<p>$key : $word </p>";
+}
+?>
+<hr>
+<h3>Đoạn văn bản chứa ký tự <?php echo $characters ?> được bôi đậm: </h3>
+<p><?php echo $result['paragraph_characters'] ?></p>
+<hr>
+<h3>Đoạn văn bản với các từ chứa <?php $characters ?> được bôi đậm :</h3>
+<p><?php echo $result['paragraph_words']?></p>
+<?php else: ?>
+	<p style="color: red"> <?php echo $result['error']?></p>
+<?php endif;?>
